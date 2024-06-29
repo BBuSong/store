736b7d5580fc3af663baf4d86cda854a8d6b049b
minhyo <minhyo17@naver.com>
2024-06-29 22:19:41 +0900

    기본 구조

---
+++ src/main/java/zerobase/store/StoreApplication.java
@@ -0,0 +1,20 @@
+package zerobase.store;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaAuditing
+@EnableJpaRepositories
+@SpringBootApplication
+public class StoreApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(StoreApplication.class, args);
+
+    }
+
+}
